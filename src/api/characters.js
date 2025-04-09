4a4db129cb34a590cafbced2b900e64384ec578f
Yasmin <yp8075497@gmail.com>
2025-04-09 13:24:05 -0300

    fix: correções na chamada de api

--- src/api/characters.js
+++ src/api/characters.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 // Importa a biblioteca axios, usada para fazer requisições HTTP
 
 export function fetchAllCharacters() {
   // Exporta uma função que busca todos os personagens da API
-  return axios.get('https://stranger-things-api.fly.dev/api/v1/characters');
+  return axios.get("https://stranger-things-api.fly.dev/api/v1/characters");
   // Faz uma requisição GET para a API do Stranger Things e retorna a promessa (Promise)
 }
